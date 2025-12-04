a134f33ae72a6566da6d65869424a825975602b9
M Hoffman <mhoffmanCRG>
2025-12-04 11:42:11 +0200

    86c0pdan5 - Cleanup

--- test.sh
+++
@@ -1,8 +0,0 @@
-echo '{"Status":"START","Uxd":"27/10/2025","Uxt":"07:42:22.000","Eid":"UNIClientApp","Hnm":"pc123","Fid":"LoadCustomerData","Tid":"abc6124"}' | nc -u -w0 127.0.0.1 20000
-echo '{"Status":"SUCCESS","Uxd":"27/10/2025","Uxt":"07:42:22.000","Eid":"UNIClientApp","Hnm":"pc123","Fid":"LoadCustomerData","Tid":"abc6124"}' | nc -u -w0 127.0.0.1 20000
-
-echo '{"Status":"START","Uxd":"27/10/2025","Uxt":"07:42:22.000","Eid":"UNIClientApp","Hnm":"pc123","Fid":"LoadCustomerData","Tid":"abc123"}' | nc -u -w0 127.0.0.1 20000
-sleep 1
-echo '{"Status":"INFO","Uxd":"27/10/2025","Uxt":"07:42:22.000","Eid":"UNIClientApp","Hnm":"pc123","Fid":"LoadCustomerData","Tid":"abc123"}' | nc -u -w0 127.0.0.1 20000
-sleep 2
-echo '{"Status":"SUCCESS","Uxd":"27/10/2025","Uxt":"07:42:22.000","Eid":"UNIClientApp","Hnm":"pc123","Fid":"LoadCustomerData","Tid":"abc123"}' | nc -u -w0 127.0.0.1 20000
